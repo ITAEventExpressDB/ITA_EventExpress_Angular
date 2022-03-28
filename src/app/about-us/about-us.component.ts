777199287dccbd5266b692d71f5eef6c6be2a1db
Depesi <anton.tsar02@gmail.com>
2022-03-28 15:57:18 +0300

    create component AboutUs, wrote resetStyles, connected new font

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+interface aboutUsItems{
+	img: string;
+	text: string;
+}
+
+const aboutUsMapper:aboutUsItems[] =[
+	{ img: '../../assets/images/aboutUs/aboutImg-1.png', text: "Search events" },
+	{ img: '../../assets/images/aboutUs/aboutImg-2.png', text: "Create your event" },
+	{ img: '../../assets/images/aboutUs/aboutImg-3.png', text: "Find events" },
+    { img: '../../assets/images/aboutUs/aboutImg-4.png', text: "Get connected" },
+    { img: '../../assets/images/aboutUs/aboutImg-5.png', text: "Have fun together" },
+]
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.css']
+  styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent implements OnInit {
+  ngOnInit(): void {}
+  
+  mapper = aboutUsMapper;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  joinUs(): void {
+	  console.log("join us");
   }
-
 }
