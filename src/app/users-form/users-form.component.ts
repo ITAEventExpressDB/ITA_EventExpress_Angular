ca16d40ddc04e999b7f04f5e587e3d354e9249d3
Depesi <anton.tsar02@gmail.com>
2022-04-04 13:38:40 +0300

    feat(users-form): create delete and update action, refactor html

--- src/app/users-form/users-form.component.ts
+++ src/app/users-form/users-form.component.ts
@@ -1,27 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import {UsersService} from '../services/users.service'
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-users-form',
   templateUrl: './users-form.component.html',
-  styleUrls: ['./users-form.component.scss']
+  styleUrls: ['./users-form.component.scss'],
 })
-
-
 export class UsersFormComponent implements OnInit {
-  users:any;
+  title = 'Add';
+  users: any;
+  router = 'users';
+  editing = { val: false, title: 'Add', id: '' };
 
-  constructor(private user:UsersService) { 
-	this.user.getData("users", "").subscribe(data => {
-		this.users=data;
-	})
-  }
-  getUserFormData(data:any) {
-	this.user.addUser("users", "AddUser", data).subscribe(result => {
-		console.log(result);
-	})
-  }
-  ngOnInit(): void {
+  constructor(private user: UsersService) {
+    this.getUsers();
   }
 
+  cancel() {
+    this.editing = { val: false, title: 'Add', id: '' };
+  }
+
+  getUsers() {
+    this.user.getData(this.router, '').subscribe((data) => {
+      this.users = data;
+    });
+  }
+
+  getUserFormData(data: any) {
+    (this.editing.val
+      ? this.user.editUnit(this.router, 'EditUsername', this.editing.id, data)
+      : this.user.addUser(this.router, 'AddUser', data)
+    ).subscribe(() => {
+      this.getUsers();
+      this.cancel();
+    });
+  }
+
+  deleteUser(id: string) {
+    this.user.deleteElement(this.router, 'DeleteUser', id).subscribe(() => {
+      this.getUsers();
+    });
+  }
+
+  editUsername(id: string) {
+    this.editing = { val: true, title: 'Edit', id };
+  }
+
+  ngOnInit(): void {}
 }
